017ab3b19208438aa60e5838ac5807569c0e6d36
YN <yoeri.nijsen@googlemail.com>
2015-08-26 18:06:11 +0200

    Removed j2.5 comp

--- admin/templates/elements/catmenu.php
+++ admin/templates/elements/catmenu.php
@@ -1,14 +1,8 @@
 
-	if(version_compare(JVERSION,'3','<')){
-		JSubMenuHelper::addEntry(
-			JText::_('##firstname##'),
-			'index.php?option=com_categories&extension=com_##component##.##extension##',
-			($vName == 'categories.##extension##')
-		);
-	} else {
-		JHtmlSidebar::addEntry(
-			JText::_('##firstname##'),
-			'index.php?option=com_categories&extension=com_##component##.##extension##',
-			($vName == 'categories.##extension##')
-		);
-	} 
+
+	JSubMenuHelper::addEntry(
+		JText::_('##firstname##'),
+		'index.php?option=com_categories&extension=com_##component##.##extension##',
+		($vName == 'categories.##extension##')
+	);
+	
--- admin/templates/elements/menu.php
+++ admin/templates/elements/menu.php
@@ -1,13 +1,7 @@
-	if(version_compare(JVERSION,'3','<')){
-		JSubMenuHelper::addEntry(
-			JText::_('##Plural##'),
-			'index.php?option=com_##component##&view=##plural##',
-			($vName == '##plural##')
-		);	
-	} else {
-		JHtmlSidebar::addEntry(
-			JText::_('##Plural##'),
-			'index.php?option=com_##component##&view=##plural##',
-			($vName == '##plural##')
-		);	
-	}
+	
+	JSubMenuHelper::addEntry(
+		JText::_('##Plural##'),
+		'index.php?option=com_##component##&view=##plural##',
+		($vName == '##plural##')
+	);	
+	
