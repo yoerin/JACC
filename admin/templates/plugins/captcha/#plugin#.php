168da435714027a9b936642ea9a11976ba74f0a0
YN <yoeri.nijsen@googlemail.com>
2015-09-03 19:31:47 +0200

    Captcha plugin
    Added captcha plugin

---
+++ admin/templates/plugins/captcha/#plugin#.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version		$Id: #plugin#.php 147 2013-10-06 08:58:34Z michel $
+ * @copyright	Copyright (C) ##year##, ##author##. All rights reserved.
+ * @license ###license##
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
+
+class plg##Plugtype####Plugin## extends JPlugin
+{
+
+	//Just examples
+
+	/**
+
+	public function onInit($id = 'dynamic_recaptcha_1')
+	{
+		// Do nothing
+		return true;
+	}
+
+	public function onDisplay($name = null, $id = 'dynamic_recaptcha_1', $class = '')
+	{
+		return '<div id="' . $id . '" ' . $class . '></div>';
+	}
+	public function onCheckAnswer($code = null)
+	{
+		// return true/false
+	}
+	**/
+
+}
