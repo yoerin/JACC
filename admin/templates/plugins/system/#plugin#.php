84e29f01bb4f2cff8e9d9a89523f16b56eb4fa7c
YN <yoeri.nijsen@googlemail.com>
2015-09-03 19:34:43 +0200

    System plugin
    Moved some stuff around and added some functions

--- admin/templates/plugins/system/#plugin#.php
+++ admin/templates/plugins/system/#plugin#.php
@@ -12,35 +12,60 @@ jimport('joomla.plugin.plugin');
 class plg##Plugtype####Plugin## extends JPlugin
 {
 
-    //Just examples
+	//Just examples
 
-    /**
+	/**
 
-    public function onAfterInitialise()
-    {
-        //Do something
-    }
-    public function onAfterRoute()
-    {
-        //Do something
-    }
+	public function onAfterInitialise()
+	{
+		//Do something
+	}
+	public function onAfterRoute()
+	{
+		//Do something
+	}
 
-    public function onAfterDispatch()
-    {
-        //Do something
-    }
+	public function onAfterDispatch()
+	{
+		//Do something
+	}
 
-    public function onAfterRender()
-    {
-        //Do something
-    }
+	public functon onBeforeRender()
+	{
+		//Do something
+	}
 
-    public function onUserLoginFailure($response) {
-    	print $response['error_message'];
-    	print $response['status'];
-    	print $response['username'];
-    	print $response['type'];
-    }
+	public function onAfterRender()
+	{
+		//Do something
+	}
+
+	public function onBeforeCompileHead()
+	{
+		// Do something
+	}
+
+	public function onSearch( $options = array( $searchword='', $searchphrase='', $ordering='', $areas='' ) )
+	{
+		// return $params;
+	}
+
+	public function onSearchAreas()
+	{
+		// return  array( 'categories' => 'Categories' );
+	}
+
+	public function onGetWebServices()
+	{
+		// Do nothing
+	}
+
+	public function onUserLoginFailure($response) {
+		print $response['error_message'];
+		print $response['status'];
+		print $response['username'];
+		print $response['type'];
+	}
 
 	public function onUserLogout($user, $options = array())
 	{
