0fdbbba610cac6be2d40655cf3ca167c80654e44
YN <yoeri.nijsen@googlemail.com>
2015-09-03 19:47:27 +0200

    Module update
    Updated module code for default values and added advanced params

--- admin/templates/modules/mod_#module#.php
+++ admin/templates/modules/mod_#module#.php
@@ -10,8 +10,15 @@
 defined('_JEXEC') or die('Restricted access'); // no direct access
 
 require_once __DIR__ . '/helper.php';
-$item = mod##Module##Helper::getItem($params);
-require(JModuleHelper::getLayoutPath('mod_##module##'));
-require_once ('helper.php');
+
+if ($params->def('prepare_content', 1))
+{
+	JPluginHelper::importPlugin('content');
+	$module->content = JHtml::_('content.prepare', $module->content, '', '##Module##.content');
+}
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require JModuleHelper::getLayoutPath('##Module##', $params->get('layout', 'default'));
 
 ?>
