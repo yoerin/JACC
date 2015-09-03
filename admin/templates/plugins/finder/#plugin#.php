986a2e2c4698470005d629ba707120c931a58dfe
YN <yoeri.nijsen@googlemail.com>
2015-09-03 19:33:48 +0200

    Add finder plugin
    Added finder plugin, still needs some work

---
+++ admin/templates/plugins/finder/#plugin#.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @version		$Id: #plugin#.php 147 2013-10-06 08:58:34Z michel $
+ * @copyright	Copyright (C) ##year##, ##author##. All rights reserved.
+ * @license ###license##
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+use Joomla\Registry\Registry;
+
+require_once JPATH_ADMINISTRATOR . '/components/com_finder/helpers/indexer/adapter.php';
+
+class plg##Plugtype####Plugin## extends FinderIndexerAdapter
+{
+
+    //Just an example
+
+	/**
+	 * Smart Search adapter for com_content.
+	 *
+	 * @since  2.5
+	 */
+	class PlgFinderContent extends FinderIndexerAdapter
+	{
+	/**
+	 * Method to update the item link information when the item category is
+	 * changed. This is fired when the item category is published or unpublished
+	 * from the list view.
+	 *
+	 * @param   string   $extension  The extension whose category has been updated.
+	 * @param   array    $pks        A list of primary key ids of the content that has changed state.
+	 * @param   integer  $value      The value of the state that the content has been changed to.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 */
+	public function onFinderCategoryChangeState($extension, $pks, $value)
+	{
+		// Do nothing
+	}
+
+	/**
+	 * Method to remove the link information for items that have been deleted.
+	 *
+	 * @param   string  $context  The context of the action being performed.
+	 * @param   JTable  $table    A JTable object containing the record to be deleted
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onFinderAfterDelete($context, $table)
+	{
+		// Do nothing
+	}
+
+	/**
+	 * Smart Search after save content method.
+	 * Reindexes the link information for an article that has been saved.
+	 * It also makes adjustments if the access level of an item or the
+	 * category to which it belongs has changed.
+	 *
+	 * @param   string   $context  The context of the content passed to the plugin.
+	 * @param   JTable   $row      A JTable object.
+	 * @param   boolean  $isNew    True if the content has just been created.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onFinderAfterSave($context, $row, $isNew)
+	{
+		// Do nothing
+	}
+
+	/**
+	 * Smart Search before content save method.
+	 * This event is fired before the data is actually saved.
+	 *
+	 * @param   string   $context  The context of the content passed to the plugin.
+	 * @param   JTable   $row      A JTable object.
+	 * @param   boolean  $isNew    If the content is just about to be created.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onFinderBeforeSave($context, $row, $isNew)
+	{
+		// Do nothing
+	}
+
+	/**
+	 * Method to update the link information for items that have been changed
+	 * from outside the edit screen. This is fired when the item is published,
+	 * unpublished, archived, or unarchived from the list view.
+	 *
+	 * @param   string   $context  The context for the content passed to the plugin.
+	 * @param   array    $pks      An array of primary key ids of the content that has changed state.
+	 * @param   integer  $value    The value of the state that the content has been changed to.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 */
+	public function onFinderChangeState($context, $pks, $value)
+	{
+		// We only want to handle articles here.
+		if ($context == 'com_content.article' || $context == 'com_content.form')
+		{
+			$this->itemStateChange($pks, $value);
+		}
+
+		// Handle when the plugin is disabled.
+		if ($context == 'com_plugins.plugin' && $value === 0)
+		{
+			$this->pluginDisable($pks);
+		}
+	}
+}
